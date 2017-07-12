b5be0ca8cc01b54f05cd148e284058659a7752fe
RussTorres <russtorres@gmail.com>
2017-07-12 16:53:09 -0700

    update requirements, PEP8

--- renderflask/dict_utils.py
+++ renderflask/dict_utils.py
@@ -1,20 +1,23 @@
 from collections import Mapping
-from itertools import chain
 from operator import add
 import collections
 _FLAG_FIRST = object()
 
-def flattenDict(d, join=add, lift=lambda x:x):
+
+def flattenDict(d, join=add, lift=lambda x: x):
     results = []
+
     def visit(subdict, results, partialKey):
-        for k,v in subdict.items():
-            newKey = lift(k) if partialKey==_FLAG_FIRST else join(partialKey,lift(k))
-            if isinstance(v,Mapping):
+        for k, v in subdict.iteritems():
+            newKey = lift(k) if partialKey == _FLAG_FIRST else join(
+                partialKey, lift(k))
+            if isinstance(v, Mapping):
                 visit(v, results, newKey)
             else:
-                results.append((newKey,v))
+                results.append((newKey, v))
     visit(d, results, _FLAG_FIRST)
     return results
+
 
 def flatten(d, parent_key='', sep='_'):
     items = []
